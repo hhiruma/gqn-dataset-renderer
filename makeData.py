8df13b645d6ccd018c4d195329ffab1631c44f93
Noon <hyohyogo@yahoo.co.jp>
2019-01-18 22:08:36 +0900

    fix makeData

--- makeData.py
+++ makeData.py
@@ -1,8 +1,9 @@
+import argparse
 import numpy as np
 import os
 import random
 import cv2
-import pandas as pd
+import math
 
 def main():
     img_data_list = []
@@ -10,8 +11,8 @@ def main():
 
     for scene in os.listdir(args.input_directory):
 
-        images_dir_path = os.path.join(scene, "images")
-        viewpoints_dir_path = os.path.join(scene, "viewpoints")
+        images_dir_path = os.path.join(args.input_directory, scene, "images")
+        viewpoints_dir_path = os.path.join(args.input_directory, scene, "viewpoints")
         image_path_list = os.listdir(images_dir_path)
         viewpoint_path_list = os.listdir(viewpoints_dir_path)
 
@@ -20,13 +21,15 @@ def main():
 
         for i in random.sample(range(len(image_path_list)), args.num_views_per_scene):
             img = cv2.imread(os.path.join(images_dir_path, image_path_list[i]))
-            clipped_img = img[:,(img.shape[1]-img.shape[0])/2:((img.shape[1]-img.shape[0])/2 + img.shape[0]),:]
+            clipped_img = img[:,int((img.shape[1]-img.shape[0])/2):int(((img.shape[1]-img.shape[0])/2 + img.shape[0])),:]
             scaled_img = cv2.resize(clipped_img, dsize=(64,64))
             img_data_list_per_scene.append(scaled_img)
 
             with open(os.path.join(viewpoints_dir_path, viewpoint_path_list[i])) as f:
                 line = f.read()
-                viewpoint = [int(s) for s in line.split(',')]
+                vp = [float(s) for s in line.split(',')]
+                viewpoint = [vp[0], vp[1], vp[2], math.sin(vp[3]), math.cos(vp[3]), math.sin(vp[4]), math.cos(vp[4])]
+
                 vp_data_list_per_scene.append(viewpoint)
 
         img_data_list.append(img_data_list_per_scene)
